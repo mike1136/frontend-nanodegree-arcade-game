b66bc8610facec17eff8153fb4f95a164604438f
Michael Valentin <Michael.Valentin1136@gmail.com>
2018-06-09 14:00:56 -0400

    Version 0.0.1
    research: Read the Comments to figure the code.
    research: Figured how to render enemies.
    fix: Added enemiesAll Array.
    fix: Added Player class.
    fix:Translated function Enemy to Class

--- js/app.js
+++ js/app.js
@@ -1,40 +1,59 @@
 // Enemies our player must avoid
-var Enemy = function() {
+class Enemy {
+
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
-    this.sprite = 'images/enemy-bug.png';
+    constructor(sprite = 'images/enemy-bug.png', x = 1, y = 2.3, speed = 400) {
+        this.sprite = sprite;
+        this.x = x;
+        this.y = y;
+        this.speed = speed;
+    }
+    /**
+     * Update the enemy's position, required method for game by multiplying dt by speed
+     * @param {Date} dt a time delta between ticks
+     */
+    update(dt) {
+        this.x += (dt * this.speed);
+
+    }
+    /**Draw the enemy on the screen, required method for game
+     * 
+     */
+    render() {
+        ctx.drawImage(Resources.get(this.sprite), this.x, this.y * 25);
+    }
 };
 
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
-};
 
-// Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
 
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+class playerP {
+    constructor() {}
+    update() {}
+    render() {}
+    handleInput() {}
+};
 
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
+var enemy1 = new Enemy();
+var allEnemies = [enemy1];
+console.log(enemy1.sprite);
+var player = new playerP();
 
 
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
-document.addEventListener('keyup', function(e) {
+document.addEventListener('keyup', function (e) {
     var allowedKeys = {
         37: 'left',
         38: 'up',
@@ -43,4 +62,5 @@ document.addEventListener('keyup', function(e) {
     };
 
     player.handleInput(allowedKeys[e.keyCode]);
+    console.log(e.keyCode);
 });
